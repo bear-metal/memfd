a2e43b860c154c8b1b7191755f88e3cae7e81f3c
Lourens Naudé <lourens@methodmissing.com>
2017-12-04 00:49:19 +0000

    Prefer running benchmarks with a pool of clients

--- bench/perf.rb
+++ bench/perf.rb
@@ -14,47 +14,64 @@ memfd.seal!
 
 iterations = 10000
 
-Benchmark.bmbm do |results|
-  results.report("Standard UNIX sockets") do
-    path = "/tmp/memfd-#{SecureRandom.hex}"
-    server = UNIXServer.new(path)
-    pid = fork do
-      client = UNIXSocket.new(path)
-      while true do
-        client.read(payload_size)
-      end
-      client.close
+path = "/tmp/memfd-#{SecureRandom.hex}"
+server = UNIXServer.new(path)
+5.times do
+  fork do
+    p "-> client #{Process.pid}"
+    client = UNIXSocket.new(path)
+    while true do
+      client.read(payload_size)
     end
-    server_socket = server.accept
-     
-    iterations.times do
-      server_socket.write(payload)
-    end
-    server_socket.close
-  end
-
-  results.report("Zero-copy transfer") do
-    path = "/tmp/memfd-#{SecureRandom.hex}"
-    server = Memfd::Server.new(path)
-    server.memfd = memfd
-
-    pid = fork do
-      client = Memfd::Client.new(path)
-      while true do
-        begin
-          client.read
-        rescue SocketError
-          break
-        end
-      end
-      client.close
-    end
-
-    server.accept
-     
-    iterations.times do
-      server.write
-    end
-    server.close
+    client.close
+    p "<- client #{Process.pid}"
   end
 end
+
+sockets = []
+5.times { sockets << server.accept }
+
+path = "/tmp/memfd-#{SecureRandom.hex}"
+memfd_server = Memfd::Server.new(path)
+memfd_server.memfd = memfd
+
+5.times do
+  fork do
+    memfd_client = Memfd::Client.new(path)
+    p "-> memfd client #{Process.pid}"
+    while true do
+      begin
+        memfd_client.read
+      rescue SocketError
+        break
+      end
+    end
+    memfd_client.close
+    p "<- memfd client #{Process.pid}"
+  end
+end
+
+5.times { memfd_server.accept }
+
+sleep 2
+
+Benchmark.bmbm do |results|
+  results.report("Standard UNIX sockets") do
+    iterations.times do
+      sockets.each do |sock|
+        sock.write(payload)
+      end
+    end
+  end
+
+  results.report("memfd zero-copy transfer") do
+    iterations.times do
+      memfd_server.write
+    end
+  end
+end
+
+sockets.each(&:close)
+server.close
+
+sleep 1
